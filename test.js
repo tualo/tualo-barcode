369831485ce829ba675ead8d572611dce7114eb8
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2013-05-31 09:10:09 +0200

    added dependencies, updated the docs

--- test.js
+++ test.js
@@ -1,25 +1,23 @@
 var http = require('http');
 var Barcode = require('./lib/barcode').Barcode;
 
-
-
 var server = http.createServer(function(req, res) {
 	res.writeHead(200);
-	var svg = (new Barcode({
+	var code_39_svg = (new Barcode({
 		type: 'Code39',
 		width: 300
 	})).getSVG('tualo.de',true);
 
-	var svg2 = (new Barcode({
+	var datamatrix_svg = (new Barcode({
 		type: 'Datamatrix',
 		fillColor: 'blue',
 		strokeColor: 'darkblue',
 		strokeWidth: 1,
 		width: 'auto',
 		height: 'auto'
-	})).getSVG('ehrgfevfzefdhewhvfdgebfghefdeb vfdghg34zurt7364 34udf34v hgcqewf tualo.de',true);
+	})).getSVG('http://tualo.de',true);
 	
-	res.end('<html><body style="background: white;">'+svg+'<br/><br/>'+svg2+'</body></html>');
+	res.end('<html><body style="background: white;">'+code_39_svg+'<br/><br/>'+datamatrix_svg+'</body></html>');
 });
 
 server.listen(parseInt(process.env.PORT) || 3000);
